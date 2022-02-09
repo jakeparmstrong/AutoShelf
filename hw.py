dfc296c91f7b86740dfb8d173fe43fb661884762
Jacob <jake.p.armstrong@gmail.com>
2022-02-08 23:05:50 -0500

    shelf array logic written

--- hw.py
+++ hw.py
@@ -1,29 +1,95 @@
 from tkinter import *
 from tkinter import ttk
 
-shelves = [False, False, False] # 3 shelves. F for empty, T for full
+NUM_SHELVES = 3
 
-def store_handler():
+def init():
+  #initialize some global variables
+  global shelves
+  global free_space_count
+  global shelf_buttons
+  global frm
+  free_space_count = NUM_SHELVES
+  shelves = [False for i in range(NUM_SHELVES)] # 3 shelves. F for empty, T for full
+  shelf_buttons = []
+  for i in range(NUM_SHELVES):
+    btn = make_active_btn(i)
+    #btn = ttk.Button(frm, text="Storage Space " + str(i), command=lambda: retrieve_item(i))
+    btn.grid(column=0, row=i, sticky="news")
+    btn.grid_remove()
+    shelf_buttons.append(btn)
+
+def store_btn_handler():
+  global shelves
+  global free_space_count
   print("Storing Item")
-  # storage_spot = find_storage_space()
-  # result = store_in(storage_spot)
-  # if result == ...
+  if free_space_count == 0:
+    return False
+  else:
+    for idx,item in enumerate(shelves):
+      if item == False:
+        #if store_in(spot):
+        shelves[idx] = True
+        free_space_count -= 1
+        return True
 
-def retrieve_handler():
+def retrieve_item(item_spot):
+  global free_space_count
+  global shelves
+  print(shelves)
+  #if retrieve_from(item_spot): //if returns true
+  print(item_spot)
+  shelves[item_spot] = False
+  free_space_count += 1
+  print(shelves)
+
+  for item in shelf_buttons:
+    item.grid_remove()
+    store_button.grid()
+  retrieve_button.grid()
+  
+
+def make_active_btn(idx):
+  b = ttk.Button(frm, text="Storage Space " + str(idx), command=lambda: retrieve_item(idx))
+  b.grid(column=0, row=idx, sticky="news")
+  return b
+
+def retrieve_btn_handler():
   print("Retrieving item")
+  global store_button
+  global retrieve_button
+  
+  #hide buttons temporarily
+  store_button.grid_remove()
+  retrieve_button.grid_remove()
+  for idx,item in enumerate(shelf_buttons):
+    if shelves[idx] == True: #Something is there
+      item.state(["!disabled"])  
+      item.grid()
+      #make_active_btn(item)
+    else: #Nothing is there
+      #b = ttk.Button(frm, text="Storage Space " + str(item))
+      #b.grid(column=0, row=item, sticky="news")
+      item.grid()
+      item.state(["disabled"])  
+  #hide buttons, new buttons
+
 
 root = Tk()
-root.config(cursor="none")
+# TODO uncomment for touchscreen
+#root.config(cursor="none")
 root.rowconfigure(0, weight=1)
 root.columnconfigure(0, weight=1)
 root.attributes("-fullscreen", True)
 frm = ttk.Frame(root, padding=10)
-frm.grid()
+frm.grid(sticky="news")
+init()
 
-store_button = ttk.Button(frm, text="Store Item", command=store_handler)
+
+store_button = ttk.Button(frm, text="Store Item", command=store_btn_handler)
 store_button.grid(column=0, row=0, sticky="news")
 
-retrieve_button = ttk.Button(frm, text="Retrieve Item", command=retrieve_handler)
+retrieve_button = ttk.Button(frm, text="Retrieve Item", command=retrieve_btn_handler)
 retrieve_button.grid(column=0, row=1, sticky="nesw")
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=2, sticky="enws")
+#ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=2, sticky="enws")
 root.mainloop()
