a21d26d62c43821722553d59331e1ef295345e30
Jacob <jake.p.armstrong@gmail.com>
2022-02-28 17:33:36 -0500

    uploading tested photoresistor class

--- Photoresistor.py
+++ Photoresistor.py
@@ -2,4 +2,6 @@ import RPi.GPIO as GPIO
 from DigitalSensor import DigitalSensor
 
 class Photoresistor(DigitalSensor):
-    pass
+    def __init__(self, input_pin_num):
+        super().__init__(input_pin_num)
+    
